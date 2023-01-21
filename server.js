e146a8c48190dab65348b27e714155cdb67ff596
tarun nahak <tarunnahak25@gmail.com>
2023-01-22 00:48:58 +0530

    added routes for bootcamp

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const dotenv = require("dotenv");
 // Load the .env file
 dotenv.config({ path: "./config/config.env" });
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
+const app = express();
+// mount router
+app.use("/api/v1/bootcamps", bootcamps);
 
 app.listen(
   PORT,
